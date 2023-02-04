ff3d52e5e885af0910e96ebce21c80f0a3f00f7e
Tringa Krasniqi <tringe.krs#gmail.com>
2023-02-04 12:59:19 +0100

    Perundimi i Session 4

--- Sessions and HW/4rth Session - 2-4-23/e_app_two/app.js
+++ Sessions and HW/4rth Session - 2-4-23/e_app_two/app.js
@@ -13,12 +13,26 @@ let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+//set ndryshon dicka. Per variablen view me bashku me dirname
+//dirname variabel globale qe na e then komplet path qe aplikacionit
+//views i qaset folderit view
 app.set('view engine', 'jade');
+//jade engine - temp engine din me komuniku me server side edhe me client
 
 app.use(logger('dev'));
+//use eshte main middleware me app.use thirren middleware
+/*middleware ne esence eshte metode amo bohet run para metodave psh user, controller. 
+e pranon req, res edhe next qe i ka parametra. edhe ne mundemi me kriju middleware. 
+nese e kthejme nje req middleware kthen res nje mesazh. 
+edhe performanca(se kur vine plot req palidhje ne i kthejme pa u testu kshtu minimizon kohen) edhe siguria bohet me middleware.
+next metoda thot nese gjithcka shkon mire vazhdo ne metoda.*/
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+//localhost:3000 -domain, /test - parameter
+//domain nuk kemi nevoje me encode se nuk kan api '/', '.', '""', etj. 
+//Kto simbolika encoder i konverton me nje simbol qe nuk e len me rrezu serverin apo si ben dem serverit.
 app.use(cookieParser());
+//
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
