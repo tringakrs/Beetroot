87396e52e6b1c29f43691f1d3b436c5b8ae3de10
Tringa Krasniqi <tringe.krs#gmail.com>
2023-02-07 18:35:51 +0100

    Session 5 1st commit

--- Sessions and HW/4rth Session - 2-4-23/e_app_two/app.js
+++ Sessions and HW/4rth Session - 2-4-23/e_app_two/app.js
@@ -37,6 +37,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//routes jane lokacionet nga rrejdhin requests
+//
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Sessions and HW/4rth Session - 2-4-23/e_app_two/routes/index.js
+++ Sessions and HW/4rth Session - 2-4-23/e_app_two/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', session:'Session 5' });//shtimi per testim i session
 });
 
 module.exports = router;
