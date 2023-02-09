73b93f873099bba9e160946872072f5c605e35dd
Tringa Krasniqi <tringe.krs#gmail.com>
2023-02-09 19:44:50 +0100

    Session6-create branch, fs,SOLID princ,write,read

--- Sessions and HW/4rth Session - 2-4-23/e_app_two/routes/index.js
+++ Sessions and HW/4rth Session - 2-4-23/e_app_two/routes/index.js
@@ -1,9 +1,64 @@
 var express = require('express');
 var router = express.Router();
+//Session 6 - thirrja e file system
+const fs = require('fs');
+//Me i listu
+//console.log(fs);
 
 /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express', session:'Session 5' });//shtimi per testim i session 5
+
+//WRITE FILE
+//   fs.writeFile('text1.txt', 'Hello World!', function (err){
+//     if(err){
+//       console.log(err);
+//     }else{
+//       console.log("File written successfully");
+//     }
+//   })
+
+// });
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', session:'Session 5' });//shtimi per testim i session
+  res.render('index', { title: 'Express', session:'Session 5' });//shtimi per testim i session 5
+
+  //WRITE FILE REFACTOR
+  fs.writeFile('text1.txt', 'Hello World!', function (err){
+    if(err){
+      console.log(err);
+      return;
+    }
+    console.log("File written successfully");
+  })
+
+  //READ FILE KTHEN STRING
+  // fs.readFile('text1.txt', function(err, data){
+  //   if(err){
+  //     console.log(err);
+  //     return;
+  //   }
+  //   console.log(data);
+  // })
+
+  // fs.readFile('text1.txt', 'utf8', function(err, data){
+  //   if(err){
+  //     console.log(err);
+  //     return;
+  //   }
+  //   console.log(data);
+  // })
+
+  //toString() mnyra tjeter per me kthy tekstin
+  fs.readFile('text1.txt', function(err, data){
+    if(err){
+      console.log(err);
+      return;
+    }
+    console.log(data.toString());
+  })
+
 });
+
 
 module.exports = router;
