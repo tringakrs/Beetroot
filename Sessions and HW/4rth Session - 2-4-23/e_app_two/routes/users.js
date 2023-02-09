73117e120f600a948d1041cddc2d3f1238a472ff
Tringa Krasniqi <tringe.krs#gmail.com>
2023-02-09 18:35:43 +0100

    Session 6 - Krijimi i branch

--- Sessions and HW/4rth Session - 2-4-23/e_app_two/routes/users.js
+++ Sessions and HW/4rth Session - 2-4-23/e_app_two/routes/users.js
@@ -7,51 +7,54 @@ router.get('/', function(req, res, next) {
 //Session 5
 const obj1 = {name: 'Tringa'};
 const array1 = ['tringa', 'krasniqi']
-console.log(obj1);
+// console.log(obj1);
 
-//ARRAY
-array1.push("banana", "apple", "peach");
-console.log(array1.length);
+// //ARRAY
+// array1.push("banana", "apple", "peach");
+// console.log(array1.length);
 
-const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
-console.log(animals.slice(2));
+// const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+// console.log(animals.slice(2));
 
-const reversed = array1.reverse();
-console.log('reversed:', reversed);
+// const reversed = array1.reverse();
+// console.log('reversed:', reversed);
 
-const array2 = [1, 4, 9, 16];
-const map1 = array2.map(x => x * 2);
-console.log(map1);
+// const array2 = [1, 4, 9, 16];
+// const map1 = array2.map(x => x * 2);
+// console.log(map1);
 
-const words = ['tringa', 'krasniqi', 'dicka', 'tjeter', 'whatever', 'nodejs'];
-const result = words.filter(word => word.length > 6);
-console.log(result);
+// const words = ['tringa', 'krasniqi', 'dicka', 'tjeter', 'whatever', 'nodejs'];
+// const result = words.filter(word => word.length > 6);
+// console.log(result);
 
-const months = ['March', 'Jan', 'Feb', 'Dec'];
-months.sort();
-console.log(months);
+// const months = ['March', 'Jan', 'Feb', 'Dec'];
+// months.sort();
+// console.log(months);
 
-const stuff = ['laps', 'kimik', 'vizore', 'banke'];
-console.log(animals.lastIndexOf('banke'));
+// const stuff = ['laps', 'kimik', 'vizore', 'banke'];
+// console.log(animals.lastIndexOf('banke'));
 
-//OBJEKTI
-const obj2 = {
-  a: 'string',
-  b: 42,
-  c: false
-};
-console.log(Object.keys(obj2));
-console.log(Object.values(obj2));
+// //OBJEKTI
+// const obj2 = {
+//   a: 'string',
+//   b: 42,
+//   c: false
+// };
+// console.log(Object.keys(obj2));
+// console.log(Object.values(obj2));
 
-console.log(Object.is('1', 1));
+// console.log(Object.is('1', 1));
 
-const obj3 = {
-  property1: 42
-};
+// const obj3 = {
+//   property1: 42
+// };
 
-console.log(Object.isFrozen(obj3));
-Object.freeze(obj3);
-console.log(Object.isFrozen(obj3));
+// console.log(Object.isFrozen(obj3));
+// Object.freeze(obj3);
+// console.log(Object.isFrozen(obj3));
+
+//Session 6
+console.log({array1});
 
   res.send('respond with a resource');
 });
---
+++ Sessions and HW/6th Session - 2-3-23/README.txt
@@ -0,0 +1,21 @@
+Global variable psh log
+Global variables are variables that are accessible from anywhere in the program, 
+regardless of the location of the code that wants to use them. 
+In other words, a global variable has a scope that extends throughout the entire program, 
+allowing it to be used by functions and code blocks defined in different parts of the program.
+In most programming languages, global variables are declared outside of any function, 
+and they can be accessed and modified by any function or code block in the program. 
+The use of global variables can make it easier to share data between different parts of a program, 
+but they can also make it more difficult to understand the behavior of the program, 
+and they can lead to unintended side effects if they are not used carefully.
+It's generally considered a best practice to avoid using global variables whenever possible, 
+and to instead use local variables that have a more limited scope. When global variables are necessary, 
+it's important to document their use and behavior clearly, to minimize the risk of unintended consequences.
+
+//Me shti array ne Objekt
+console.log({array});
+
+//Me kriju branch te ri 
+git checkout -b feature/session6
+//Me i shiku branch-at
+git branch
