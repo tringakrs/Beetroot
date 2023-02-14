0c5ea897008702e4997340fb3f8c11eb61833de6
Tringa Krasniqi <tringe.krs#gmail.com>
2023-02-14 19:11:18 +0100

    Session 7 - Pjesa e pare - PATH

--- Sessions and HW/4rth Session - 2-4-23/e_app_two/routes/index.js
+++ Sessions and HW/4rth Session - 2-4-23/e_app_two/routes/index.js
@@ -2,8 +2,51 @@ var express = require('express');
 var router = express.Router();
 //Session 6 - thirrja e file system
 const fs = require('fs');
+const { dirname } = require('path');
 //Me i listu
 //console.log(fs);
+
+//Session 7 - path
+const path= require('path');
+
+console.log(path);
+
+//Path metodat
+const pathTestDir = path.basename(__dirname);
+const pathTestFile = path.basename(__filename);
+const pathTestExt = path.extname(__filename);//me bo dirname nuk na kthen se folder nuk ka extension
+
+console.log(pathTestDir);
+console.log(pathTestFile);
+console.log(pathTestExt);
+
+var obj = { dir: 'C:\\Users\\Refsnes', base: 'demo_path.js' }
+
+var p = path.format(obj);
+console.log(p);
+
+console.log(path.parse('/home/user/dir/file.txt'));
+
+var x = path.normalize('Users/Refsnes/../Jackson');
+console.log(x);
+
+console.log(path.resolve('wwwroot', 'static_files/png/', '../gif/image.gif'));
+
+console.log(path.isAbsolute('/foo/bar'));
+console.log(path.isAbsolute('/baz/..'));
+console.log(path.isAbsolute('qux/'));
+console.log(path.isAbsolute('.'));
+
+console.log(path.join('/foo', 'bar', 'baz/asdf', 'quux', '..'));
+// Returns: '/foo/bar/baz/asdf'
+
+//path.join('foo', {}, 'bar');
+// Throws 'TypeError: Path must be a string. Received {}'
+
+
+
+
+
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
@@ -23,7 +66,7 @@ const fs = require('fs');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', session:'Session 5' });//shtimi per testim i session 5
 
-  //WRITE FILE REFACTOR
+/*  //WRITE FILE REFACTOR
   fs.writeFile('text1.txt', 'Hello World!', function (err){
     if(err){
       console.log(err);
@@ -70,8 +113,7 @@ fs.rename('text1.txt', 'newFileName.txt', function(err) {
   if (err) throw err;
   console.log('File Renamed!');
 });
-
+*/
 });
-
 
 module.exports = router;
